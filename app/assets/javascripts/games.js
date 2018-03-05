1107a8ea68e9771e59693365163fbde7842380f1
Ian Harwell <sirharwell@gmail.com>
2018-03-05 10:53:50 -0700

    add javascript

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -1,2 +1,19 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+var currentGame = {}
+
+$(document).ready( function() {
+  $('.game-item').on('click', function() {
+    currentGame.id = this.dataset.id;
+    $.ajax({
+      url: '/games/' + currentGame.id + '/characters',
+      type: 'GET',
+      dataType: 'JSON'
+    }).done( function(characters) {
+      var list = $('#characters');
+      list.empty();
+      characters.forEach( function(char) {
+        var li = '<li data-character-id="' + char.id + '">' + char.name + ' - ' + char.power + '</li>'
+        list.append(li)
+      });
+    });
+  });
+});
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def render_error(model)
-    render json: { errors: model.errors.full_messages.join)','}, status: 422
+    render json: { errors: model.errors.full_messages.join(',')}, status: 422
   end
 end
